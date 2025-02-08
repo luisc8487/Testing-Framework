1036d717ce9ee07c44be4b801074d32df03827ce
L C <LF9904@gmail.com>
2025-02-07 20:48:59 -0500

    collecting test files

--- runner.js
+++ runner.js
@@ -1,10 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 class Runner {
     constructor() {
-        this.files = [];    
+        this.testFiles = [];    
     }
 
-    collectFiles() {
-        
+    async collectFiles(targetPath) {
+        const files = fs.readdirSync(targetPath);
+
+        for(let file of files) {
+            const filePath = path.join(targetPath, file);
+            const stats = await fs.promises.lstat(filePath);
+
+            if(stats.isFile() && file.includes('.test.js')) {
+                this.testFiles.push({name: filePath});
+            }else if(stats.isDirectory()) {
+                const childFiles = await fs.promises.readdir(filePath);
+
+                files.push(...childFiles.map(f => path.join(file, f)));
+            }
+        }
     }
 }
 
