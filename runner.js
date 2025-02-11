0a7335bcd84807c06c04aef72a8ef28562d9b487
L C <LF9904@gmail.com>
2025-02-11 12:30:30 -0500

    builded a render function

--- runner.js
+++ runner.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const chalk = require("chalk");
+const render = require("./render");
 
 const forbiddenDirs = ["node_modules"];
 class Runner {
@@ -12,6 +13,7 @@ class Runner {
     for (let file of this.testFiles) {
       console.log(chalk.gray(`------- ${file.shortName}`));
       const beforeEaches = [];
+      global.render = render;
       global.beforeEach = (fn) => {
         beforeEaches.push(fn);
       };
